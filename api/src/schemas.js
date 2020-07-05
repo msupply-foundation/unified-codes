cb3a76bd85c48a569a3bfc3f2202f87d458b73e9
Will Thomson <will.tho@outlook.com>
2020-07-05 21:00:52 +1200

    Prettify existing code

--- api/src/schemas.js
+++ api/src/schemas.js
@@ -1,35 +1,29 @@
-import S from "fluent-schema";
+import S from 'fluent-schema';
 
 const healthSchema = {
   response: {
-    200: S.object().prop("mysql", S.string()).prop("api", S.string()),
+    200: S.object().prop('mysql', S.string()).prop('api', S.string()),
   },
 };
 
 const itemsSchema = {
-  queryString: S.object().prop("code", S.string()),
+  queryString: S.object().prop('code', S.string()),
   response: {
     200: S.object(),
-    404: S.object().prop("error", S.string()),
+    404: S.object().prop('error', S.string()),
   },
 };
 
 const versionSchema = {
   response: {
-    200: S.object()
-      .prop("version", S.string())
-      .prop("versionShort", S.string()),
+    200: S.object().prop('version', S.string()).prop('versionShort', S.string()),
   },
 };
 
-export {
-  healthSchema as health,
-  itemsSchema as items,
-  versionSchema as version
-};
+export { healthSchema as health, itemsSchema as items, versionSchema as version };
 
 export default {
   health: healthSchema,
   items: itemsSchema,
-  version: versionSchema
+  version: versionSchema,
 };
--- api/test/api.test.js
+++ api/test/api.test.js
@@ -5,125 +5,140 @@ import { apiResponses, graphResponses, data } from './fixtures';
 import api from '../src/api';
 import queries from '../src/queries';
 
-describe("Test health endpoint", () => {
-    test("Health endpoint response has status code 200", done => {
-        api.inject({
-            method: 'GET',
-            url: '/health'
-        }).then(response => {
-            const { statusCode } = response;
-            expect(statusCode).toBe(200);
-            done();
-        });
-    });
+describe('Test health endpoint', () => {
+  test('Health endpoint response has status code 200', (done) => {
+    api
+      .inject({
+        method: 'GET',
+        url: '/health',
+      })
+      .then((response) => {
+        const { statusCode } = response;
+        expect(statusCode).toBe(200);
+        done();
+      });
+  });
 });
 
-describe("Test items endpoint", () => {
-    test("Items endpoint with no params has status code 200", done => {
-        nock("http://localhost:8080")
-          .post('/query')
-          .query(queryObject => !!queryObject.timeout)
-          .reply(200, graphResponses.items.all);
-        api.inject({
-            method: 'GET',
-            url: '/items'
-        }).then(response => {
-            const { statusCode } = response;
-            expect(statusCode).toBe(200);
-            done();
-        });
-    });
+describe('Test items endpoint', () => {
+  test('Items endpoint with no params has status code 200', (done) => {
+    nock('http://localhost:8080')
+      .post('/query')
+      .query((queryObject) => !!queryObject.timeout)
+      .reply(200, graphResponses.items.all);
+    api
+      .inject({
+        method: 'GET',
+        url: '/items',
+      })
+      .then((response) => {
+        const { statusCode } = response;
+        expect(statusCode).toBe(200);
+        done();
+      });
+  });
 
-    test("Items endpoint with no params returns all items", done => {
-        nock('http://localhost:8080')
-            .post('/query')
-            .query(queryObject => !!queryObject.timeout)
-            .reply(200, graphResponses.items.all);
-        api.inject({
-            method: 'GET',
-            url: '/items',
-        }).then(response => {
-            const { body } = response;
-            expect(body).toBe(JSON.stringify(apiResponses.items.all));
-            done();
-        });
-    });
+  test('Items endpoint with no params returns all items', (done) => {
+    nock('http://localhost:8080')
+      .post('/query')
+      .query((queryObject) => !!queryObject.timeout)
+      .reply(200, graphResponses.items.all);
+    api
+      .inject({
+        method: 'GET',
+        url: '/items',
+      })
+      .then((response) => {
+        const { body } = response;
+        expect(body).toBe(JSON.stringify(apiResponses.items.all));
+        done();
+      });
+  });
 
-    test("Items endpoint with valid code returns matching item", done => {
-        const { items } = data;
-        const [item] = items;
-        const { code } = item;
+  test('Items endpoint with valid code returns matching item', (done) => {
+    const { items } = data;
+    const [item] = items;
+    const { code } = item;
 
-        nock("http://localhost:8080")
-            .post('/query')
-            .query(queryObject => !!queryObject.timeout)
-            .reply(200, (_, requestBody) => {
-              const { query, variables } = requestBody;
-              const validQuery = query === queries.items.get;
-              const validVariables = variables.code === code;
-              if (validQuery && validVariables) {
-                return graphResponses.items[code];
-              }
-            });
+    nock('http://localhost:8080')
+      .post('/query')
+      .query((queryObject) => !!queryObject.timeout)
+      .reply(200, (_, requestBody) => {
+        const { query, variables } = requestBody;
+        const validQuery = query === queries.items.get;
+        const validVariables = variables.code === code;
+        if (validQuery && validVariables) {
+          return graphResponses.items[code];
+        }
+      });
 
-        api.inject({
-            method: 'GET',
-            url: `/items?code=${code}`
-        }).then(response => {
-            const { body } = response;
-            expect(body).toBe(JSON.stringify(apiResponses.items[code]));
-            done();
-        });
-    });
+    api
+      .inject({
+        method: 'GET',
+        url: `/items?code=${code}`,
+      })
+      .then((response) => {
+        const { body } = response;
+        expect(body).toBe(JSON.stringify(apiResponses.items[code]));
+        done();
+      });
+  });
 
-    test("Items endpoint with invalid params has status code 400", done => {
-        api.inject({
-            method: 'GET',
-            url: '/items',
-            query: ({ badQuery: 'invalidData' })
-        }).then(response => {
-            const { statusCode } = response;
-            expect(statusCode).toBe(400);
-            done();
-        });
-    });
-
+  test('Items endpoint with invalid params has status code 400', (done) => {
+    api
+      .inject({
+        method: 'GET',
+        url: '/items',
+        query: { invalidParam: 'invalidData' },
+      })
+      .then((response) => {
+        const { statusCode } = response;
+        expect(statusCode).toBe(400);
+        done();
+      });
+  });
 });
 
-describe("Test version endpoint", () => {
-    test("Version endpoint response has status code 200", done => {
-        api.inject({
-            method: 'GET',
-            url: '/version'
-        }).then(response => {
-            const { statusCode } = response;
-            expect(statusCode).toBe(200);
-            done();
-        });
-    });
+describe('Test version endpoint', () => {
+  test('Version endpoint response has status code 200', (done) => {
+    api
+      .inject({
+        method: 'GET',
+        url: '/version',
+      })
+      .then((response) => {
+        const { statusCode } = response;
+        expect(statusCode).toBe(200);
+        done();
+      });
+  });
 
-    test("Version endpoint response has body with version properties", done => {
-        api.inject({
-            method: 'GET',
-            url: '/version'
-        }).then(response => {
-            const body = response.json();
-            expect(body).toHaveProperty('version');
-            expect(body).toHaveProperty('versionShort');
-            done();
-        });
-    });
+  test('Version endpoint response has body with version properties', (done) => {
+    api
+      .inject({
+        method: 'GET',
+        url: '/version',
+      })
+      .then((response) => {
+        const body = response.json();
+        expect(body).toHaveProperty('version');
+        expect(body).toHaveProperty('versionShort');
+        done();
+      });
+  });
 });
 
-describe("Test invalid endpoint", () => {
-    test("Invalid endpoint response has status code 404", done => {
-        api.inject({
-            method: 'GET',
-            url: '/invalid'
-        }).then(response => {
-            const { statusCode } = response;
-            expect(statusCode).toBe(404);
-            done();
-        });
-    });
+describe('Test invalid endpoint', () => {
+  test('Invalid endpoint response has status code 404', (done) => {
+    api
+      .inject({
+        method: 'GET',
+        url: '/invalid',
+      })
+      .then((response) => {
+        const { statusCode } = response;
+        expect(statusCode).toBe(404);
+        done();
+      });
+  });
 });
