e43743fbd56cce4b2c628e7cab40e05ce4b5c31f
Will Thomson <will.tho@outlook.com>
2020-07-06 02:30:29 +1200

    Tidy up dgraph queries

--- api/src/queries.js
+++ api/src/queries.js
@@ -1,23 +1,22 @@
 const items = {
   get: `{
-        query(func: has(code)) @filter(eq(code, $code)) {	
-          code
-          description
-        }
-      }
-    }`,
+    query(func: has(code)) @filter(eq(code, $code)) {	
+      code
+      description
+    }
+  }`,
   search: `{
-        query(func: has(code)) @filter(allofterms(description, "$search")) {
-          code
-          description
-        }
-    }`,
+    query(func: has(code)) @filter(allofterms(description, "$search")) {
+      code
+      description
+    }
+  }`,
   all: `{
-        query(func: has(code)) @filter(eq(type,"unit_of_use")) {
-            code
-            description
-        }
-    }`,
+    query(func: has(code)) @filter(eq(type,"unit_of_use")) {
+      code
+      description
+    }
+  }`,
 };
 
 export { items };
