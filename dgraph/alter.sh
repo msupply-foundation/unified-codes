3b4cf4a690c0ca994236e440944ba115ac8fcaf5
katherine-sussol <katherine@sussol.net>
2020-07-07 14:09:25 +1200

    Update schema to be GQL compliant

--- dgraph/alter.sh
+++ dgraph/alter.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -s -H "Content-Type: application/graphql+-" "localhost:8080/alter?commitNow=true" -XPOST --data-binary "@$1" | python3 -m json.tool
+curl -X POST localhost:8080/admin/schema --data-binary "@$1"
