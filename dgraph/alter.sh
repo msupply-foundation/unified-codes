91785982786a9dd71d17b9beb46d1475ab5aada2
katherine-sussol <katherine@sussol.net>
2020-07-07 16:03:23 +1200

    Re-add -s arg to alter.sh

--- dgraph/alter.sh
+++ dgraph/alter.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -H "Content-Type: application/graphql" "localhost:8080/admin/schema" -XPOST --data-binary "@$1"
+curl -s -H "Content-Type: application/graphql" "localhost:8080/admin/schema" -XPOST --data-binary "@$1"
