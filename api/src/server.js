fa89cd3b76bd2275878dbf4061129c6f2be68b1e
katherine-sussol <katherine@sussol.net>
2020-07-13 17:26:43 +1200

    Revert "Add apollo server to api project"
    This reverts commit ef79153f0d149ae85c8dd71bb1f0b646a6d8e4bf.

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,8 @@
 import api from './api.js';
-import graphApi from './graphql/api.js'
 
 const start = async () => {
   try {
     await api.listen(3000);
-    await graphApi.listen(4000);
   } catch (err) {
     api.log.error(err);
     process.exit(1);
