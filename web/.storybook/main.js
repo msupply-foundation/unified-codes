d1366a9ff93fe9ca1beced1dc00143b743855df3
Will Thomson <will.tho@outlook.com>
2020-07-15 12:45:09 +1200

    Migrate storybook to typescript

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -1,4 +1,19 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-};
+  stories: ['../stories/**/*.stories.tsx'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+        // Optional
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
+  };
