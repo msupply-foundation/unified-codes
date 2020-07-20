799dd012cb348e545e9f1748a9d5dbce04f5b18f
Will Thomson <will.tho@outlook.com>
2020-07-20 20:28:00 +1200

    Add EntityTableHeader stories

---
+++ web/stories/EntityTableHeader.stories.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { EntityTableHeader } from '../src/components';
+
+export default { title: 'EntityTableHeader' };
+
+export const withNoProps = () => {
+    return <EntityTableHeader/>;
+}
