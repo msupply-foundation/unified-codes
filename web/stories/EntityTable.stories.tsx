fdfb7412b9170ef90a28b9c2124082c689613a17
Will Thomson <will.tho@outlook.com>
2020-07-21 12:57:01 +1200

    Add EntityTable story

---
+++ web/stories/EntityTable.stories.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Entity } from '../src/types';
+import { EntityTable } from '../src/components';
+
+const entities: Entity[] = [
+    {
+        "code": "QFWR9789",
+        "description": "Amoxicillin",
+        "type": "medicinal_product",
+    },
+    {
+        "code": "GH89P98W",
+        "description": "Paracetamol",
+        "type": "medicinal_product",
+    }
+];
+
+export default { title: 'EntityTable' };
+
+export const withAmoxicillinAndParacetamol = () => {
+    return <EntityTable entities={entities}/>;
+}
