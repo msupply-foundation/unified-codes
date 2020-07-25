05ac086f9aeb13914c2ba5c9ab03b865e3060ff3
Will Thomson <will.tho@outlook.com>
2020-07-25 21:33:27 +1200

    Tidy up components index file

--- web/src/components/index.tsx
+++ web/src/components/index.tsx
@@ -5,5 +5,10 @@ export { Container, ContainerProps } from './atoms';
 export { Input, InputProps } from './atoms';
 export { SearchButton, SearchButtonProps } from './atoms';
 export { SearchIcon, SearchIconProps } from './atoms';
+
+export { EntityTableRow, EntityTableRowProps } from './molecules'; 
+export { EntityTableHeader, EntityTableHeaderProps } from './molecules'; 
 export { InputField, InputFieldProps } from './molecules';
 export { SearchBar, SearchBarProps } from './molecules';
+
+export { EntityTable, EntityTableProps } from './organisms';
