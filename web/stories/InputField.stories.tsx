1b524aef1c22b4021140090a1985f4f284fd13a9
Will Thomson <will.tho@outlook.com>
2020-07-25 21:31:51 +1200

    Add InputField stories

---
+++ web/stories/InputField.stories.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { ClearIcon, SearchIcon, InputField } from '../src/components';
+
+export default { title: 'InputField' };
+
+export const withCancelIcon = () => <InputField icon={ClearIcon}/>;
+export const withSearchIcon = () => <InputField icon={SearchIcon}/>;
