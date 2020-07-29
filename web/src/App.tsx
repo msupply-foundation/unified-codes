73a104914503efc904c3b5141bb988b1f0b1e512
Will Thomson <will.tho@outlook.com>
2020-07-29 15:05:32 +1200

    Prettify web project root src files

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,9 +4,13 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { MainPage } from "./pages";
 
 export const App = () => {
-    const client = new ApolloClient({
-        uri: 'http://localhost:4000/graphql',
-        cache: new InMemoryCache(),
-    });
-    return <ApolloProvider client={client}><MainPage/></ApolloProvider>;
+  const client = new ApolloClient({
+    uri: "http://localhost:4000/graphql",
+    cache: new InMemoryCache(),
+  });
+  return (
+    <ApolloProvider client={client}>
+      <MainPage />
+    </ApolloProvider>
+  );
 };
