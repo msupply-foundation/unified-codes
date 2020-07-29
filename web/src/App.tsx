b1125a551507f21f6ac64df385afa8d60fae9960
Mark Prins <mark@malcolmprins.net>
2020-07-29 17:13:54 +1200

    set auth header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,36 @@
 import * as React from "react";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
 
 import { MainPage } from "./pages";
 
 export const App = () => {
+    const httpLink = createHttpLink({
+        uri: '/graphql',
+      });
+
+    interface Context {
+        headers?: Record<string, string>;
+    }
+    type ContextSetter = (operation: any, prevContext: Context) => Promise<Context> | any;
+    const contextSetter:ContextSetter = (_, { headers }) => {
+        // get the authentication token from local storage if it exists
+        const token = localStorage.getItem('token');
+        // return the headers to the context so httpLink can read them
+        return {
+            headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+            }
+        }
+    } 
+    const authLink = setContext(contextSetter);
+
     const client = new ApolloClient({
+        // TODO: lift the URL out of here
         uri: 'http://localhost:4000/graphql',
         cache: new InMemoryCache(),
+        link: authLink.concat(httpLink),
     });
     return <ApolloProvider client={client}><MainPage/></ApolloProvider>;
 };
