0b69bfb27ea79342b7722cd2e5343584b4e478bd
Will Thomson <will.tho@outlook.com>
2020-08-01 07:56:04 +1200

    Fix buggy storybook config

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -6,11 +6,7 @@ module.exports = {
       use: [
         {
           loader: require.resolve('ts-loader'),
-        },
-        // Optional
-        {
-          loader: require.resolve('react-docgen-typescript-loader'),
-        },
+        }
       ],
     });
     config.resolve.extensions.push('.ts', '.tsx');
