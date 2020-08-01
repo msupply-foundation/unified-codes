89fecab6a3e0f4f343b1a9a5d7fcfe97ae93a5cb
Will Thomson <will.tho@outlook.com>
2020-08-02 09:08:05 +1200

    Add Login template story

---
+++ web/stories/Login.stories.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+import { Login } from "../src/components/templates";
+
+export default { title: "Login" };
+
+export const withNoProps = () => {
+    const client = new ApolloClient({
+        uri: "http://localhost:4000/graphql",
+        cache: new InMemoryCache(),
+      });
+
+      return (
+        <ApolloProvider client={client}>
+            <Login/>
+        </ApolloProvider>
+      );
+} 
