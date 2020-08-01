6306feac77436604ebd89f2d3446237ff1dc9a46
Will Thomson <will.tho@outlook.com>
2020-08-02 09:06:55 +1200

    Add temp routing interface to app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,80 @@
 import * as React from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { AppBar, Button, Grid, Container, IconButton, Toolbar, Typography, VisibilityIcon } from "./components/atoms";
+import { Explorer, Login } from "./components/templates";
 
-import { MainPage } from "./pages";
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    link: { color: "white" },
+  })
+);
 
 export const App = () => {
+  const classes = useStyles();
+
   const client = new ApolloClient({
     uri: "http://localhost:4000/graphql",
     cache: new InMemoryCache(),
   });
+
   return (
-    <ApolloProvider client={client}>
-      <MainPage />
-    </ApolloProvider>
+    <Router>
+      <Container>
+        <Grid container spacing={3} direction="column" justify="space-between" alignItems="stretch">
+          <Grid container item>
+            <AppBar position="static">
+              <Toolbar>
+                <Grid container alignItems="center">
+                    <Grid container item xs={11} alignItems="center">
+                      <Grid item>
+                        <Link to="/explorer" className={classes.link}>
+                          <IconButton edge="start" color="inherit" aria-label="menu">
+                            <VisibilityIcon/>
+                          </IconButton>
+                        </Link>
+                      </Grid>
+                      <Grid item>
+                        <Typography variant="h6">
+                          Explorer
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                    <Grid container item xs={1} alignItems="center">
+                      <Button color="inherit" aria-label="login"><Link to="/login" className={classes.link}>Login</Link></Button>
+                    </Grid>
+                  </Grid>
+              </Toolbar>
+            </AppBar>
+          </Grid>
+          <Grid container item>
+            <Switch>
+              <Route exact path="/explorer">
+                <ApolloProvider client={client}>
+                    <Explorer/>
+                </ApolloProvider>
+              </Route>
+              <Route exact path="/login">
+                <ApolloProvider client={client}>
+                    <Login/>
+                </ApolloProvider>
+              </Route>
+              <Route>
+                <Redirect to="/explorer"/>
+              </Route>
+            </Switch>
+          </Grid>
+        </Grid>
+      </Container>
+    </Router>
   );
 };
+
+export default App;
---
+++ web/src/components/atoms/core/Toolbar.tsx
@@ -0,0 +1,1 @@
+export { Toolbar, ToolbarProps } from "@material-ui/core";
