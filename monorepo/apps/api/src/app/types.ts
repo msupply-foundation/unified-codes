088339a26e8e355059c316e599a1abd3a8d3b0e9
katherine-sussol <katherine@sussol.net>
2020-08-03 14:20:55 +1200

    Fix api fastify types after dependency alignment

--- monorepo/apps/api/src/app/types.ts
+++ monorepo/apps/api/src/app/types.ts
@@ -1,16 +1,17 @@
 import Dgraph from 'dgraph-js-http';
-import Fastify from 'fastify';
-import Http from 'http';
+import * as Fastify from 'fastify';
+import Http, { ServerResponse } from 'http';
+import { JSONSchema } from 'fluent-schema';
 
 export type Api = Fastify.FastifyInstance<Http.Server, Http.IncomingMessage, Http.ServerResponse>;
-export type Handler = Fastify.RequestHandler;
-export type Query = Fastify.DefaultQuery;
-export type Reply = Fastify.FastifyReply<Http.ServerResponse>;
+export type Handler = Fastify.RouteHandler;
+export type Query = Fastify.RequestQuerystringDefault;
+export type Reply = Fastify.FastifyReply<Fastify.RawServerBase>;
 export type Request = Fastify.FastifyRequest;
-export type Schema = Fastify.RouteSchema & {
+export type Schema = Fastify.FastifySchema & {
   response: {
-    [code: number]: Fastify.JSONSchema;
-    [code: string]: Fastify.JSONSchema;
+    [code: number]: JSONSchema;
+    [code: string]: JSONSchema;
   };
 };
 export type DgraphResponse = Dgraph.Response & { data: { query: [] } };
