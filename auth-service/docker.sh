54ab5ab76a460c54b707483df4163c3c25ffbe0a
Mark Prins <mark@malcolmprins.net>
2020-08-03 13:17:25 +1200

    change from import to scripts

--- auth-service/docker.sh
+++ auth-service/docker.sh
@@ -1,1 +1,1 @@
-docker run -p 9990:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e KEYCLOAK_IMPORT=/tmp/unified-codes.json --mount type=bind,source="$(pwd)",target=/tmp jboss/keycloak
+docker run -p 9990:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin jboss/keycloak
