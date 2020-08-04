1454167da383e0cefd83ba8eeda55275077be653
Will Thomson <will.tho@outlook.com>
2020-08-04 21:42:10 +1200

    Refactor LoginForm component

--- web/src/components/organisms/core/LoginForm.tsx
+++ web/src/components/organisms/core/LoginForm.tsx
@@ -1,12 +1,32 @@
 import * as React from "react";
-import { Grid } from "../../atoms"
-import { LoginButtons, LoginInput } from "../../molecules";
 
-export const LoginForm = () => {
+import { Button, Grid, Typography } from "../../atoms"
+import { LoginInput } from "../../molecules";
+
+export interface LoginFormProps {
+    onSubmit?: (username: string, password: string) => void;
+}
+
+export type LoginForm = React.FunctionComponent<LoginFormProps>;
+
+export const LoginForm: LoginForm = ({ onSubmit }) => {
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    const onChange = React.useCallback(({ username, password }) => {
+        setUsername(username);
+        setPassword(password);
+    }, []);
+
+    const onClick = React.useCallback(() => onSubmit ? onSubmit(username, password) : null, [username, password, onSubmit]);
+
     return (
-        <Grid container direction="column" spacing={2} alignItems="center">
-            <Grid item><LoginInput/></Grid>
-            <Grid item><LoginButtons/></Grid>
+        <Grid>
+            <Grid item><Typography align="center" display="block" variant="button">Login</Typography></Grid>
+            <Grid container direction="column" spacing={3} alignItems="stretch">
+                <Grid item><LoginInput username={username} password={password} onChange={onChange}/></Grid>
+                <Grid item><Button fullWidth onClick={onClick}>Submit</Button></Grid>
+            </Grid>
         </Grid>
     )
 };
