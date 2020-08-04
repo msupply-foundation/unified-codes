40e1c806fea0773b0659615ba34cfee8a9d61aec
Will Thomson <will.tho@outlook.com>
2020-08-04 21:40:15 +1200

    Add PasswordInput component

--- web/src/components/molecules/core/index.tsx
+++ web/src/components/molecules/core/index.tsx
@@ -1,4 +1,5 @@
 export { IconInput, StandardIconInputProps, FilledIconInputProps, OutlinedIconInputProps } from "./IconInput";
 export { LoginInput } from "./LoginInput";
+export { PasswordInput, PasswordInputProps } from "./PasswordInput";
 export { SearchBar, SearchBarProps } from "./SearchBar";
 export { UsernameInput, UsernameInputProps } from "./UsernameInput";
