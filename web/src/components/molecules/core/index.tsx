19ea52c097d5f325ae3f60332f437e0d54315906
Will Thomson <will.tho@outlook.com>
2020-08-04 21:39:35 +1200

    Add IconInput component

--- web/src/components/molecules/core/index.tsx
+++ web/src/components/molecules/core/index.tsx
@@ -1,4 +1,4 @@
-export { LoginButtons } from "./LoginButtons";
+export { IconInput, StandardIconInputProps, FilledIconInputProps, OutlinedIconInputProps } from "./IconInput";
 export { LoginInput } from "./LoginInput";
 export { SearchBar, SearchBarProps } from "./SearchBar";
 export { MenuBar } from "./MenuBar";
