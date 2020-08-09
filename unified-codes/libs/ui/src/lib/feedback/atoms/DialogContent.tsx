074b8a25a4a9a378eea180711241d3ca2158a93f
Will Thomson <will.tho@outlook.com>
2020-08-09 18:11:59 +1200

    Add ui library feedback components

---
+++ unified-codes/libs/ui/src/lib/feedback/atoms/DialogContent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { DialogContent as MDialogContent, DialogContentProps as MDialogContentProps } from "@material-ui/core";
+
+export type DialogContentProps = MDialogContentProps;
+
+export type DialogContent = React.FunctionComponent<DialogContentProps>;
+
+export const DialogContent: DialogContent = props => <MDialogContent {...props}></MDialogContent>;
+
+export default DialogContent;
