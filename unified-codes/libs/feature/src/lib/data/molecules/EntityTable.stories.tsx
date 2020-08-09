2905b41fcbfdbe95a8cfe4af637633125554ef74
Will Thomson <will.tho@outlook.com>
2020-08-09 19:34:58 +1200

    Add feature library component stories

---
+++ unified-codes/libs/feature/src/lib/data/molecules/EntityTable.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Entity, EntityNode } from '@unified-codes/util';
+
+import EntityTable from './EntityTable';
+
+export default {
+  component: EntityTable,
+  title: 'EntityTable',
+};
+
+export const primary = () => {
+  const data: EntityNode[] = [
+    {
+      code: "QFWR9789",
+      description: "Amoxicillin",
+      type: "medicinal_product",
+    },
+    {
+      code: "GH89P98W",
+      description: "Paracetamol",
+      type: "medicinal_product",
+    },
+  ];
+  const entities = data.map((entityNode: EntityNode) => new Entity(entityNode));
+  return <EntityTable data={entities} />;
+};
