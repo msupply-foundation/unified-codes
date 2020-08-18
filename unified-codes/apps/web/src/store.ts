2a0c3ca6fe460b7b5b5adf0c094979eb1a8b7bed
Will Thomson <will.tho@outlook.com>
2020-08-18 20:41:45 +1200

    Add combined reducers to store

--- unified-codes/apps/web/src/store.ts
+++ unified-codes/apps/web/src/store.ts
@@ -1,9 +1,12 @@
-import { createStore, applyMiddleware } from '@reduxjs/toolkit';
+import { applyMiddleware, createStore, combineReducers } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
-import reducers from './reducers';
+
+import { AlertReducer, UserReducer } from './reducers';
 import { rootSaga } from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
+
+const reducers = combineReducers({ alert: AlertReducer, user: UserReducer });
 
 const store = createStore(reducers, applyMiddleware(sagaMiddleware));
 
