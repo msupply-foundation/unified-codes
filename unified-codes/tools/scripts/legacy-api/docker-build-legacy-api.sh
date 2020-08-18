fedbcf39ee405ca446d1450557c0ca27b4cdbdae
katherine-sussol <katherine@sussol.net>
2020-08-18 17:03:30 +1200

    Try out nx-docker instead of shell script

--- unified-codes/tools/scripts/legacy-api/docker-build-legacy-api.sh
+++
@@ -1,3 +0,0 @@
-npm run nx build legacy-api
-
-docker build --tag legacy-api:0.1 -f ../../dockerfiles/legacy-api.Dockerfile ../../..
--- unified-codes/tools/scripts/legacy-api/docker-run-legacy-api.sh
+++ unified-codes/tools/scripts/legacy-api/docker-run-legacy-api.sh
@@ -1,4 +1,4 @@
 # Remove old container if one exists
 docker rm --force legacy-api
 
-docker run --publish 3000:3000 --detach --name legacy-api legacy-api:0.1
+docker run --publish 3000:3000 --detach --name legacy-api unified-codes/legacy-api:latest
