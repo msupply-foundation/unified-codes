fc56c17ebc357749578eb5275665d19eb1d4555d
Will Thomson <will.tho@outlook.com>
2020-08-19 20:11:05 +1200

    Fix missing reducer exports

--- unified-codes/apps/web/src/reducers/index.ts
+++ unified-codes/apps/web/src/reducers/index.ts
@@ -1,2 +1,4 @@
 export { AlertReducer } from './AlertReducer';
+export { AuthenticatorReducer } from './AuthenticatorReducer';
+export { EntityReducer } from './EntityReducer';
 export { UserReducer } from './UserReducer';
