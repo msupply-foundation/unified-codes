80a046b59011a154e16a6335099fb6ab492cf800
Will Thomson <will.tho@outlook.com>
2020-08-19 20:04:17 +1200

    Add entity reducer

---
+++ unified-codes/apps/web/src/reducers/EntityReducer.ts
@@ -0,0 +1,27 @@
+import { Entity } from '@unified-codes/data';
+import { ENTITY_ACTIONS, IEntityAction } from '../actions/EntityActions';
+import { log } from 'console';
+
+const initialState = () => {
+  const entities: Entity[] = [];
+  return entities;
+};
+
+export const EntityReducer = (state = initialState(), action: IEntityAction) => {
+  const { type } = action;
+
+  switch (type) {
+    case ENTITY_ACTIONS.UPDATE: {
+      const { data } = action;
+      const { entities } = data;
+      console.log(entities);
+
+      return entities;
+    }
+    
+    default:
+      return state;
+  }
+};
+
+export default EntityReducer;
