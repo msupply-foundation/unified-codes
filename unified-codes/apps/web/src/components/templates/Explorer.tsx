a447054734979f08c4d6afd97b7bcceeadf85306
Will Thomson <will.tho@outlook.com>
2020-08-19 20:07:52 +1200

    Refactor Explorer component

--- unified-codes/apps/web/src/components/templates/Explorer.tsx
+++ unified-codes/apps/web/src/components/templates/Explorer.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+
 import { useQuery, gql } from '@apollo/client';
 
-import { Alert, EntityBrowser, Grid, Snackbar } from '@unified-codes/ui';
-import { AlertSeverity, Entity, EntityNode, IAlert } from '@unified-codes/data';
+import { EntityBrowser, Grid } from '@unified-codes/ui';
+import { AlertSeverity, Entity, IEntity, IAlert } from '@unified-codes/data';
 
-import { useAlert } from '../../hooks';
+import EntityActions from '../../actions/EntityActions';
+import AlertActions from '../../actions/AlertActions';
 
 const ALERT_SEVERITY = {
   FETCH: AlertSeverity.info,
@@ -26,56 +30,63 @@ const query = gql`
   }
 `;
 
-export const Explorer = () => {
-  const [entities, setEntities] = React.useState<Entity[]>([]);
-  const { alert, setAlert, resetAlert } = useAlert();
+export interface ExplorerProps {
+  entities: Entity[],
+  onData: (data: { entities: IEntity[] }) => void,
+  onError: () => void,
+  onLoading: () => void
+}
+
+export type Explorer = React.FunctionComponent<ExplorerProps>;
+
+export const ExplorerComponent: Explorer = ({ entities, onData, onError, onLoading }) => {
   const { loading, error, data } = useQuery(query);
 
-  const updateAlert = React.useCallback(
-    (newAlert: IAlert) => {
-      if (
-        alert.isVisible != newAlert.isVisible ||
-        alert.severity != newAlert.severity ||
-        alert.text != newAlert.text
-      ) {
-        setAlert(newAlert);
+  React.useEffect(() => {
+    if (!entities.length) {
+      if (data) {
+        onData(data);
+      } else if (error) {
+        onError();
+      } else if (loading) {
+        onLoading();
       }
-    },
-    [alert, setAlert]
-  );
-
-  if (!entities.length) {
-    if (data) {
-      const entityData = data;
-      const entities = entityData.entities.map((entityNode: EntityNode) => new Entity(entityNode));
-      setEntities(entities);
-    } else if (error) {
-      const alertError: IAlert = {
-        isVisible: true,
-        severity: ALERT_SEVERITY.ERROR,
-        text: ALERT_TEXT.ERROR,
-      };
-      updateAlert(alertError);
-    } else if (loading) {
-      const alertFetch: IAlert = {
-        isVisible: true,
-        severity: ALERT_SEVERITY.FETCH,
-        text: ALERT_TEXT.FETCH,
-      };
-      updateAlert(alertFetch);
-    }
-  }
+    } 
+  }, [entities, loading, error, data]);
 
   return (
     <Grid container justify="center">
       <EntityBrowser entities={entities} />
-      <Snackbar open={alert.isVisible} autoHideDuration={6000} onClose={resetAlert}>
-        <Alert onClose={resetAlert} severity={alert.severity}>
-          {alert.text}
-        </Alert>
-      </Snackbar>
     </Grid>
   );
 };
+
+const mapStateToProps = (state: { entities: IEntity[] }) => {
+  const { entities: entityNodes } = state;
+  const entities = entityNodes.map((entityNode: IEntity) => new Entity(entityNode)) ?? [];
+  return { entities };
+}
+
+const mapDispatchToProps = (dispatch: Dispatch)  => {
+  const alertError: IAlert = {
+    isVisible: true,
+    severity: ALERT_SEVERITY.ERROR,
+    text: ALERT_TEXT.ERROR,
+  };
+
+  const alertFetch: IAlert = {
+    isVisible: true,
+    severity: ALERT_SEVERITY.FETCH,
+    text: ALERT_TEXT.FETCH,
+  };
+
+  const onData = (data: { entities: IEntity[] }) => dispatch(EntityActions.updateEntities(data));
+  const onError = () => dispatch(AlertActions.showAlert(alertError));
+  const onLoading = () => dispatch(AlertActions.showAlert(alertFetch));
+
+  return { onData, onError, onLoading };
+}
+
+export const Explorer = connect(mapStateToProps, mapDispatchToProps)(ExplorerComponent);
 
 export default Explorer;
