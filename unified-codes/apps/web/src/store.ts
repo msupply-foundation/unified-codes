01aab2408745f93773a7cfb99f15579614c94a8c
Will Thomson <will.tho@outlook.com>
2020-08-19 20:11:59 +1200

    Fix store missing reducer definitions

--- unified-codes/apps/web/src/store.ts
+++ unified-codes/apps/web/src/store.ts
@@ -1,12 +1,11 @@
 import { applyMiddleware, createStore, combineReducers } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
-
-import { AlertReducer, UserReducer } from './reducers';
+import { AlertReducer, AuthenticatorReducer, EntityReducer, UserReducer } from './reducers';
 import { rootSaga } from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const reducers = combineReducers({ alert: AlertReducer, user: UserReducer });
+const reducers = combineReducers({ alert: AlertReducer, user: UserReducer, entities: EntityReducer, authenticator: AuthenticatorReducer });
 
 const store = createStore(reducers, applyMiddleware(sagaMiddleware));
 
