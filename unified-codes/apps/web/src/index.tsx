dc72bc230591767dcdee920a3787f0117a8d5a20
Will Thomson <will.tho@outlook.com>
2020-08-19 20:14:01 +1200

    Update web app entry point

--- unified-codes/apps/web/src/index.tsx
+++ unified-codes/apps/web/src/index.tsx
@@ -1,6 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-import { App } from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import App from './App';
+import { store } from './store';
+
+const client = new ApolloClient({
+    uri: process.env.NX_DATA_SERVICE,
+    cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </ApolloProvider>,
+    document.getElementById('app')
+);
