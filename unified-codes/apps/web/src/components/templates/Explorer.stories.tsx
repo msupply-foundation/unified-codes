fcfe466b7c1833eb7ff682e9267454092a1731c7
Will Thomson <will.tho@outlook.com>
2020-08-19 20:10:04 +1200

    Update Explorer component story

--- unified-codes/apps/web/src/components/templates/Explorer.stories.tsx
+++ unified-codes/apps/web/src/components/templates/Explorer.stories.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
-import Explorer from './Explorer';
+import { Entity, IEntity } from '@unified-codes/data';
+
+import { ExplorerComponent } from './Explorer';
 
 export default { title: 'Explorer' };
 
@@ -11,9 +13,20 @@ export const primary = () => {
     cache: new InMemoryCache(),
   });
 
+  const [entities, setEntities] = React.useState<Entity[]>([]);
+
+  const onData = (data: { entities: IEntity[] }) => {
+    const { entities: entityNodes } = data;
+    const entities = entityNodes.map((entityNode: IEntity) => new Entity(entityNode));
+    setEntities(entities);
+  }
+
+  const onError = () => console.log("onError called...");
+  const onLoading = () => console.log("onLoading called...");
+
   return (
     <ApolloProvider client={client}>
-      <Explorer />
+      <ExplorerComponent entities={entities} onData={onData} onError={onError} onLoading={onLoading} />
     </ApolloProvider>
   );
 };
