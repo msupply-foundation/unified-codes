22a169b3ae165e3b2752f6ead78edc8d26c326c4
Will Thomson <will.tho@outlook.com>
2020-08-20 19:53:41 +1200

    Add login to AuthenticationService api

--- unified-codes/libs/data/src/lib/types/AuthenticationService.ts
+++ unified-codes/libs/data/src/lib/types/AuthenticationService.ts
@@ -2,6 +2,12 @@ import { JWTToken } from './JWT';
 import IdentityProvider from './IdentityProvider';
 import User, { IUserCredentials } from './User';
 
+export class AuthenticationError extends Error {
+  constructor(message?: string) {
+    super(message);
+  }
+}
+
 export class AuthenticationService {
   provider: IdentityProvider;
 
@@ -9,8 +15,14 @@ export class AuthenticationService {
     this.provider = provider;
   }
 
-  async authenticate(credentials: IUserCredentials): Promise<User> {
+  async login(credentials: IUserCredentials): Promise<User> {
     const token: JWTToken = await this.provider.getIdentityToken(credentials);
+    return new User(token);
+  }
+
+  async authenticate(token: JWTToken): Promise<User> {
+    const isValid = this.provider.verifyIdentityToken(token);
+    if (!isValid) throw new AuthenticationError('invalid user token');
     return new User(token);
   }
 }
