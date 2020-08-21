7c0f449740695e280bcc95d2281f92aa5f4b973e
Will Thomson <will.tho@outlook.com>
2020-08-21 21:35:24 +1200

    Remove Apollo client from web app

--- unified-codes/apps/web/src/components/templates/Explorer.tsx
+++ unified-codes/apps/web/src/components/templates/Explorer.tsx
@@ -2,109 +2,47 @@ import * as React from 'react';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 
-import { ApolloError, useLazyQuery, gql, NetworkStatus } from '@apollo/client';
-
 import { EntityBrowser, Grid } from '@unified-codes/ui';
-import { AlertSeverity, Entity, IEntity, IAlert } from '@unified-codes/data';
+import { AlertSeverity, Entity } from '@unified-codes/data';
 
-import EntityActions from '../../actions/EntityActions';
-import AlertActions from '../../actions/AlertActions';
-
-const ALERT_SEVERITY = {
-  FETCH: AlertSeverity.info,
-  ERROR: AlertSeverity.error,
-};
-
-const ALERT_TEXT = {
-  FETCH: 'Fetching...',
-  ERROR: 'Could not fetch data.',
-};
-
-const GET_ENTITIES = gql`
-  query entities {
-    entities {
-      code
-      description
-      type
-    }
-  }
-`;
+import { ExplorerActions } from '../../actions';
+import { IState } from '../../types';
+import { ExplorerSelectors } from '../../selectors';
 
 export interface ExplorerProps {
   entities: Entity[],
-  onChange: (input: string) => void,
+  onReady: () => void,
   onClear: () => void,
-  onSearch: () => void,
-  onCompleted: (data: { entities: IEntity[] }) => void,
-  onError: () => void,
-  onLoading: () => void
+  onSearch: (input: string) => void,
 }
 
 export type Explorer = React.FunctionComponent<ExplorerProps>;
 
-export const ExplorerComponent: Explorer = ({ entities, onChange, onClear, onSearch, onCompleted, onError, onLoading }) => {
-  const [getEntities, { networkStatus }] = useLazyQuery(GET_ENTITIES, { onCompleted, onError });
-
+export const ExplorerComponent: Explorer = ({ entities, onReady, onClear, onSearch }) => {
   React.useEffect(() => {
-    if (networkStatus === NetworkStatus.ready) {
-      onLoading();
-      getEntities();
-    }
-  }, [getEntities, onLoading]);
+    onReady();
+  }, []);
 
   return (
     <Grid container justify="center">
-      <EntityBrowser entities={entities} onChange={onChange} onClear={onClear} onSearch={onSearch} />
+      <EntityBrowser entities={entities} onClear={onClear} onSearch={onSearch} />
     </Grid>
   );
 };
 
 
-const mapStateToProps = (state: { entities: IEntity[] }) => {
-  const { entities: entityNodes } = state;
-  const entities = entityNodes.map((entityNode: IEntity) => new Entity(entityNode)) ?? [];
+const mapStateToProps = (state: IState) => {
+  const entities = ExplorerSelectors.entitiesSelector(state);
   return { entities };
 }
 
 const mapDispatchToProps = (dispatch: Dispatch)  => {
-  const alertError: IAlert = {
-    isVisible: true,
-    severity: ALERT_SEVERITY.ERROR,
-    text: ALERT_TEXT.ERROR,
-  };
-
-  const alertFetch: IAlert = {
-    isVisible: true,
-    severity: ALERT_SEVERITY.FETCH,
-    text: ALERT_TEXT.FETCH,
-  };
-
-  const onCompleted = (data: { entities: IEntity[] }) => dispatch(EntityActions.updateEntities(data));
-  const onError = (error: ApolloError) => { console.log(error); dispatch(AlertActions.showAlert(alertError)); }
-  const onLoading = () => dispatch(AlertActions.showAlert(alertFetch));
-
-  const onChange = () => null;
-  const onClear = () => null;
-  const onSearch = () => null;
-
-  return { onChange, onClear, onSearch, onCompleted, onError, onLoading };
+  const onClear = () => dispatch(ExplorerActions.resetVariables());
+  const onReady = () => dispatch(ExplorerActions.fetchData());
+  const onSearch = (input: string) => dispatch(ExplorerActions.updateVariables({ code: input, description: input }))
+  return { onClear, onReady, onSearch };
 }
 
 export const Explorer = connect(mapStateToProps, mapDispatchToProps)(ExplorerComponent);
 
 export default Explorer;
-
-// const resetInput = React.useCallback(() => setInput(''), []);
-// const resetData = React.useCallback(() => setData(entities), [entities]);
-
-// const onChange = React.useCallback((value) => setInput(value), []);
-// const onClear = React.useCallback(() => {
-//   resetInput();
-//   resetData();
-// }, [resetData, resetInput]);
-
-// const onSearch = React.useCallback(() => {
-//   setData(
-//     entities.filter((entity) => entity.matchesCode(input) || entity.matchesDescription(input))
-//   );
-// }, [entities, input]);
