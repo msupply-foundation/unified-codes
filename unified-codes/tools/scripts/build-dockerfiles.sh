37497094bfba09b3c0600063665b17856f70ec36
katherine-sussol <katherine@sussol.net>
2020-08-21 12:07:05 +1200

    Remove project build from script, should be separate step

--- unified-codes/tools/scripts/build-dockerfiles.sh
+++ unified-codes/tools/scripts/build-dockerfiles.sh
@@ -1,6 +1,3 @@
-# Build all affected projects
-npm run affected:build --target=build --parallel=true
-
 # Build all docker images
 docker build --rm --tag unified-codes/dependencies:latest -f ../dockerfiles/dependencies.Dockerfile ../..
 docker build --rm --tag unified-codes/legacy-api:latest -f ../dockerfiles/legacy-api.Dockerfile ../..
