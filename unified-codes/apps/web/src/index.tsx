87431c24d70e55be9081eddc5b2ad080c45d18b6
Mark Prins <mark@malcolmprins.net>
2020-08-25 12:05:36 +1200

    revert breaking change from merge

--- unified-codes/apps/web/src/index.tsx
+++ unified-codes/apps/web/src/index.tsx
@@ -2,12 +2,20 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from './muiTheme';
+
 import App from './App';
 import { store } from './store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <App />
+      </CssBaseline>
+    </ThemeProvider>
   </Provider>,
   document.getElementById('app')
 );
