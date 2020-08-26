900782f37e5a97954525ce196dc0ba88d0a2d6de
Will Thomson <will.tho@outlook.com>
2020-08-26 17:49:46 +1200

    Update keycloak scripts

--- unified-codes/tools/scripts/keycloak/run.sh
+++ unified-codes/tools/scripts/keycloak/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+KEYCLOAK_PORT=9990
+
 echo " * Launching keycloak container..."
-docker run -p 9990:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin jboss/keycloak
+docker run -p $KEYCLOAK_PORT:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin jboss/keycloak
--- unified-codes/tools/scripts/keycloak/setup.sh
+++ unified-codes/tools/scripts/keycloak/setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+KEYCLOAK_HOST=http://localhost
+KEYCLOAK_PORT=9990
+CLIENT_ID=unified-codes-data
+CLIENT_SECRET=asecretthing
+
 echo " * Submitting admin credentials..."
-RESULT=`curl --data "username=admin&password=admin&grant_type=password&client_id=admin-cli" http://localhost:9990/auth/realms/master/protocol/openid-connect/token`
+RESULT=`curl --data "username=admin&password=admin&grant_type=password&client_id=admin-cli" $KEYCLOAK_HOST:$KEYCLOAK_PORT/auth/realms/master/protocol/openid-connect/token`
 
 echo
 echo " * Recovered access token"
@@ -10,20 +15,20 @@ echo $TOKEN
 
 echo
 echo " * Creating realm..."
-curl -v http://localhost:9990/auth/admin/realms -H "Content-Type: application/json" -H "Authorization: bearer $TOKEN"   --data '{"enabled": true, "id": "unified-codes", "realm": "unified-codes"}'
+curl -v $KEYCLOAK_HOST:$KEYCLOAK_PORT/auth/admin/realms -H "Content-Type: application/json" -H "Authorization: bearer $TOKEN"   --data '{"enabled": true, "id": "unified-codes", "realm": "unified-codes"}'
 echo
 
 echo " * Creating role..."
-curl -v http://localhost:9990/auth/admin/realms/unified-codes/roles -H "Content-Type: application/json" -H "Authorization: bearer $TOKEN"   --data '{"description": "Administation access", "name": "ADMIN" }'
+curl -v $KEYCLOAK_HOST:$KEYCLOAK_PORT/auth/admin/realms/unified-codes/roles -H "Content-Type: application/json" -H "Authorization: bearer $TOKEN"   --data '{"description": "Administation access", "name": "ADMIN" }'
 
 echo
 echo " * Creating user..."
-curl -v http://localhost:9990/auth/admin/realms/unified-codes/users -H "Content-Type: application/json" -H "Authorization: bearer $TOKEN"   --data '{"username": "demo-admin", "firstName":"Demo", "lastName":"Admin", "attributes": {}, "email":"demo-admin@msupply.foundation", "emailVerified": true, "enabled":"true", "credentials": [{"type": "password", "value": "pass", "temporary": false}], "realmRoles": ["ADMIN"] }'
+curl -v $KEYCLOAK_HOST:$KEYCLOAK_PORT/auth/admin/realms/unified-codes/users -H "Content-Type: application/json" -H "Authorization: bearer $TOKEN"   --data '{"username": "demo-admin", "firstName":"Demo", "lastName":"Admin", "attributes": {}, "email":"demo-admin@msupply.foundation", "emailVerified": true, "enabled":"true", "credentials": [{"type": "password", "value": "pass", "temporary": false}], "realmRoles": ["ADMIN"] }'
 
 echo
 echo " * Creating client..."
-curl -v http://localhost:9990/auth/admin/realms/unified-codes/clients -H "Content-Type: application/json" -H "Authorization: bearer $TOKEN"   --data '{"clientId": "unified-codes-data", "enabled": true, "protocol": "openid-connect", "redirectUris": [], "rootUrl": "http://localhost:4200", "authorizationServicesEnabled": true, "bearerOnly": false, "clientAuthenticatorType": "client-secret", "publicClient": false, "serviceAccountsEnabled": true }'
+curl -v $KEYCLOAK_HOST:$KEYCLOAK_PORT/auth/admin/realms/unified-codes/clients -H "Content-Type: application/json" -H "Authorization: bearer $TOKEN"   --data '{"clientId": "$CLIENT_ID", "secret": "$CLIENT_SECRET", "enabled": true, "protocol": "openid-connect", "redirectUris": ["*"], "rootUrl": "http://localhost:4200", "webOrigins": ["*"], "authorizationServicesEnabled": true, "bearerOnly": false, "clientAuthenticatorType": "client-secret", "publicClient": false, "serviceAccountsEnabled": true }'
 
 echo " * Done."
 
-open "http://localhost:9990/auth/admin/master/console/#/realms/unified-codes"
+open "$KEYCLOAK_HOST:$KEYCLOAK_PORT/auth/admin/master/console/#/realms/unified-codes"
