de46650730a036f4e5a5498f395dcbd09a299a7d
Will Thomson <will.tho@outlook.com>
2020-08-27 17:08:01 +1200

    Add Property class to data lib

--- unified-codes/libs/data/src/lib/types/Property.ts
+++ unified-codes/libs/data/src/lib/types/Property.ts
@@ -1,7 +1,43 @@
-export type Property = {
+export interface IProperty {
   type: string;
   value: string;
-  properties?: Property[];
+  properties?: IProperty[];
 };
+
+export class Property implements IProperty {
+  private _type: string;
+  private _value: string;
+  private _properties: IProperty[];
+
+  constructor(property: IProperty) {
+    this._type = property.type;
+    this._value = property.value;
+    this._properties = property.properties;
+  }
+
+  get type(): string {
+    return this._type
+  };
+
+  get value(): string { 
+    return this._value 
+  };
+
+  get properties(): IProperty[] {
+    return this._properties 
+  };
+
+  matchesType(pattern: string) {
+    return this.type.toLowerCase().includes(pattern.toLowerCase());
+  }
+
+  matchesValue(pattern: string) {
+    return this.value.toLowerCase().includes(pattern.toLowerCase());
+  }
+
+  matchesProperty(property: Property) {
+    return this.properties?.includes(property);
+  }
+}
 
 export default Property;
