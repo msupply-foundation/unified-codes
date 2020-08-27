3b81b24149f3be0bb8d74bc9775ac043bee6604c
Mark Prins <mark@malcolmprins.net>
2020-08-28 11:59:03 +1200

    sorting

--- unified-codes/apps/web/.storybook/config.js
+++ unified-codes/apps/web/.storybook/config.js
@@ -1,7 +1,18 @@
-import { configure, addDecorator } from '@storybook/react';
+import { addDecorator, addParameters, configure } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 
 const webContext = require.context('..', true, /.stories\.(j|t)sx?$/);
 const libContext = require.context('../../../libs/ui/src/lib', true, /.stories\.(j|t)sx?$/);
+const parameters = {
+  options: {
+    storySort: {
+      method: 'alphabetical',
+      order: ['Dialog', 'Login'],
+      locales: '',
+    },
+  },
+};
+
 configure([webContext, libContext], module);
 addDecorator(withKnobs);
+addParameters(parameters);
