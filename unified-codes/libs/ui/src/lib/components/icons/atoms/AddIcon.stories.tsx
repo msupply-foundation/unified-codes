36337ff41c28e05e02bb3356356db95d4b506a31
katherine-sussol <katherine@sussol.net>
2020-08-31 17:17:21 +1200

    Add icons used in EntityType toggle

---
+++ unified-codes/libs/ui/src/lib/components/icons/atoms/AddIcon.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AddIcon } from './AddIcon';
+
+export default {
+  component: AddIcon,
+  title: 'Icons/AddIcon',
+};
+
+export const withNoProps = () => {
+  return <AddIcon />;
+};
---
+++ unified-codes/libs/ui/src/lib/components/icons/atoms/CheckCircleIcon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { CheckCircle as MCheckCircle }  from '@material-ui/icons';
+
+export interface CheckCircleIconProps {}
+
+export type CheckCircleIcon = React.FunctionComponent<CheckCircleIconProps>;
+
+export const CheckCircleIcon: CheckCircleIcon = (props) => <MCheckCircle {...props}></MCheckCircle>;
+
+export default CheckCircleIcon;
