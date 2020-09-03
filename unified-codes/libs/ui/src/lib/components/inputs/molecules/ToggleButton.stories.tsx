965ac822c14b554f10e2f5fbd6c061f4094b0a3d
Mark Prins <mark@malcolmprins.net>
2020-09-03 14:39:47 +1200

    theme refactor

--- unified-codes/libs/ui/src/lib/components/inputs/molecules/ToggleButton.stories.tsx
+++ unified-codes/libs/ui/src/lib/components/inputs/molecules/ToggleButton.stories.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from '../../../../../../../apps/web/src/muiTheme';
 
 import ToggleButton from './ToggleButton';
 
@@ -7,10 +9,14 @@ export default {
   title: 'ToggleButton',
 };
 
-export const Unselected = () => {
-  return <ToggleButton>Toggle Button</ToggleButton>;
-};
+export const Unselected = () => (
+  <ThemeProvider theme={theme}>
+    <ToggleButton>Toggle Button</ToggleButton>
+  </ThemeProvider>
+);
 
-export const Selected = () => {
-  return <ToggleButton isSelected={true}>Toggle Button</ToggleButton>;
-};
+export const Selected = () => (
+  <ThemeProvider theme={theme}>
+    <ToggleButton isSelected={true}>Toggle Button</ToggleButton>
+  </ThemeProvider>
+);
--- unified-codes/libs/ui/src/lib/components/inputs/molecules/ToggleButton.tsx
+++ unified-codes/libs/ui/src/lib/components/inputs/molecules/ToggleButton.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
+import { Theme, withStyles } from '@material-ui/core/styles';
+import { ClassNameMap } from '@material-ui/core/styles/withStyles';
 
 import Button, { ButtonProps } from '../atoms/Button';
 import AddIcon from '../../icons/atoms/AddIcon';
 import CheckCircleIcon from '../../icons/atoms/CheckCircleIcon';
-
-const styles = {
-  toggleButtonPrimary: {
-    backgroundColor: '#2B83A1',
+const getStyles = (theme: Theme) => ({
+  textPrimary: {
+    backgroundColor: theme.palette.action.active,
+    color: theme.palette.text.primary,
   },
-  toggleButtonSecondary: {
-    backgroundColor: '#5CCDF4',
+  textSecondary: {
+    backgroundColor: theme.palette.action.selected,
+    color: theme.palette.text.primary,
   },
-};
+});
 
 export interface ToggleButtonProps extends ButtonProps {
+  classes: ClassNameMap<any>;
   isSelected?: boolean;
 }
 
@@ -21,9 +25,9 @@ export type ToggleButton = React.FunctionComponent<ToggleButtonProps>;
 
 export const ToggleButton: ToggleButton = (props: ToggleButtonProps) => {
   const { isSelected } = props;
-  const buttonStyle = isSelected ? styles.toggleButtonSecondary : styles.toggleButtonPrimary;
   const startIcon = isSelected ? <CheckCircleIcon /> : <AddIcon />;
-  return <Button startIcon={startIcon} {...props} style={buttonStyle}></Button>;
+  props.color = isSelected ? 'primary' : 'secondary';
+  return <Button startIcon={startIcon} {...props}></Button>;
 };
 
-export default ToggleButton;
+export default withStyles(getStyles)(ToggleButton);
