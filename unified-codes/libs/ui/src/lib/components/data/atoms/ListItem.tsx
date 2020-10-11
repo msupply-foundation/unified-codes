36396939b300754f39c5a2b90023840950a00783
Will Thomson <will.tho@outlook.com>
2020-10-11 20:42:24 +1300

    Add list components

---
+++ unified-codes/libs/ui/src/lib/components/data/atoms/ListItem.tsx
@@ -0,0 +1,7 @@
+import { ListItem as MListItem, ListItemProps as MListItemProps } from '@material-ui/core';
+
+export type ListItemProps = MListItemProps;
+
+export const ListItem = MListItem;
+
+export default ListItem;
---
+++ unified-codes/libs/ui/src/lib/components/data/atoms/ListItemIcon.tsx
@@ -0,0 +1,7 @@
+import { ListItemIcon as MListItemIcon, ListItemIconProps as MListItemIconProps } from '@material-ui/core';
+
+export type ListItemIconProps = MListItemIconProps;
+
+export const ListItemIcon = MListItemIcon;
+
+export default ListItemIcon;
