7f94ace764625e7299744cf96c80960b3cdda2c3
Mark Prins <mark@malcolmprins.net>
2020-10-15 16:30:55 +1300

    remove warnings

--- unified-codes/apps/web/src/routes/routes.ts
+++ unified-codes/apps/web/src/routes/routes.ts
@@ -1,4 +1,7 @@
-import { DetailPage, ExplorerPage, ErrorPage, LoginPage } from '../components';
+import { DetailPage } from '../components/containers/detail';
+import { ExplorerPage } from '../components/containers/explorer';
+import { ErrorPage } from '../components/containers/error';
+import { LoginPage } from '../components/containers/login';
 
 export const ROUTES = {
     DETAIL: DetailPage,
