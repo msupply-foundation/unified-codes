eb725159264d293cd6ed94f7d4c6d59fa0807788
Kat <katherine@sussol.net>
2020-10-22 14:18:13 +1300

    Alphabetise fields

--- unified-codes/apps/data-service/src/app/schema.ts
+++ unified-codes/apps/data-service/src/app/schema.ts
@@ -21,8 +21,8 @@ import {
 
 @ObjectType()
 export class EntityType implements IEntity {
-  @Field((type) => ID)
-  uid: string;
+  @Field((type) => [EntityType], { nullable: true })
+  children: IEntity[];
 
   @Field((type) => String, { nullable: true })
   code: string;
@@ -30,17 +30,17 @@ export class EntityType implements IEntity {
   @Field((type) => String, { nullable: true })
   description: string;
 
-  @Field((type) => String, { nullable: true })
-  type: string;
-
   @Field((type) => [DrugInteractionType], { nullable: true })
   interactions: IDrugInteraction[];
 
-  @Field((type) => [EntityType], { nullable: true })
-  children: IEntity[];
-
   @Field((type) => [PropertyType], { nullable: true })
   properties: IProperty[];
+
+  @Field((type) => String, { nullable: true })
+  type: string;
+
+  @Field((type) => ID)
+  uid: string;
 }
 
 @ObjectType()
@@ -69,39 +69,39 @@ export class EntitySearchInput implements Omit<IEntitySearch, 'type'> {
 
 @InputType()
 export class EntitySortInput implements Omit<IEntitySort, 'field'> {
-  @Field((type) => String)
-  field: string;
-
   @Field((type) => Boolean)
   descending: boolean;
+
+  @Field((type) => String)
+  field: string;
 }
 
 @ObjectType()
 export class PropertyType implements IProperty {
+  @Field((type) => [PropertyType], { nullable: true })
+  properties: IProperty[];
+
   @Field((type) => String)
   type: string;
 
   @Field((type) => String)
   value: string;
-
-  @Field((type) => [PropertyType], { nullable: true })
-  properties: IProperty[];
 }
 
 @ObjectType()
 export class DrugInteractionType implements IDrugInteraction {
   @Field((type) => String)
+  description: string;
+
+  @Field((type) => String)
   name: string;
 
   @Field((type) => String)
-  description: string;
+  rxcui: string;
 
   @Field((type) => String)
   severity: string;
 
   @Field((type) => String)
   source: string;
-
-  @Field((type) => String)
-  rxcui: string;
 }
