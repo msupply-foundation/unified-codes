e66ef4ee12f1dd2bcfae93b4a41c6c137e5d48cb
Mark Prins <mark@malcolmprins.net>
2020-11-03 09:43:00 +1300

    implement match filter element

--- unified-codes/apps/data-service/src/app/types/DgraphDataSource.ts
+++ unified-codes/apps/data-service/src/app/types/DgraphDataSource.ts
@@ -7,6 +7,7 @@ import {
   IEntity,
   IEntityCollection,
 } from '@unified-codes/data';
+import { EntitySearchInput, FilterMatch } from '../schema';
 
 export class DgraphDataSource extends RESTDataSource {
   private static headers: { [key: string]: string } = {
@@ -29,12 +30,34 @@ export class DgraphDataSource extends RESTDataSource {
       }
     }`;
   }
+  private static getEntitiesFilterString(description: string, match: FilterMatch) {
+    if (!description) {
+      return '@filter(has(description))';
+    }
 
-  private static getEntitiesQuery(type, description, orderField, orderDesc, first, offset) {
+    switch (match) {
+      case 'exact':
+        return `@filter(regexp(description, /^${description}$/i))`;
+
+      case 'contains':
+        return `@filter(regexp(description, /${description}/i))`;
+
+      default:
+        return `@filter(regexp(description, /^${description}/i))`;
+    }
+  }
+
+  private static getEntitiesQuery(
+    type: string,
+    description?: string,
+    orderField?: string,
+    orderDesc?: boolean,
+    first?: number,
+    offset?: number,
+    match?: FilterMatch
+  ) {
     const orderString = `${orderDesc ? 'orderdesc' : 'orderasc'}: ${orderField}`;
-    const filterString = description
-      ? `@filter(regexp(description, /.*${description}.*/i))`
-      : '@filter(has(description))';
+    const filterString = this.getEntitiesFilterString(description, match);
 
     return `{
       all as counters(func: anyofterms(type, "${type}")) ${filterString} { 
@@ -72,19 +95,43 @@ export class DgraphDataSource extends RESTDataSource {
     return entity;
   }
 
-  async getEntities(filter, first, offset): Promise<IEntityCollection> {
-    const { type = EEntityType.DRUG, description, orderBy } = filter ?? {};
+  async getEntities(
+    filter?: EntitySearchInput,
+    first?: number,
+    offset?: number
+  ): Promise<IEntityCollection> {
+    const { type = EEntityType.DRUG, description, match, orderBy } = filter ?? {};
     const { field: orderField = EEntityField.DESCRIPTION, descending: orderDesc = true } =
       orderBy ?? {};
 
+    console.warn(
+      'query: ',
+      DgraphDataSource.getEntitiesQuery(
+        type,
+        description,
+        orderField,
+        orderDesc,
+        first,
+        offset,
+        match
+      )
+    );
     const data = await this.postQuery(
-      DgraphDataSource.getEntitiesQuery(type, description, orderField, orderDesc, first, offset)
+      DgraphDataSource.getEntitiesQuery(
+        type,
+        description,
+        orderField,
+        orderDesc,
+        first,
+        offset,
+        match
+      )
     );
 
     const { counters: countersData, query: entityData } = data ?? {};
     const [counterData] = countersData;
     const totalCount = counterData?.total;
-    
+
     // Overwrite interactions to prevent large query delays.
     const entities: IEntity[] =
       entityData?.map((entity: IEntity) => ({ ...entity, interactions: [] })) ?? [];
