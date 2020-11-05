e9cf22256b6c82b10cbddaa719fb63e70736d094
Will Thomson <will.tho@outlook.com>
2020-11-05 15:07:06 +1300

    Add PackImmediateEntity type

---
+++ unified-codes/libs/data/src/lib/types/entity/PackImmediateEntity.ts
@@ -0,0 +1,33 @@
+import { EEntityField, EEntityType, IEntity, Entity } from './Entity';
+import { IPackSizeEntity, PackSizeEntity } from './PackSizeEntity';
+import { IProperty, Property } from '../Property';
+
+export type EPackImmediateEntityField = EEntityField;
+
+export type IPackImmediateEntityChild = IPackSizeEntity;
+export type IPackImmediateEntityChildren = IPackImmediateEntityChild[];
+
+export type IPackImmediateEntityProperty = IProperty;
+export type IPackImmediateEntityProperties = IPackImmediateEntityProperty[];
+
+export interface IPackImmediateEntity extends IEntity {
+  code: string;
+  name: string;
+  type: EEntityType.PackImmediate;
+  children?: IPackImmediateEntityChild[];
+  properties?: IPackImmediateEntityProperty[];
+}
+
+export class PackImmediateEntity extends Entity implements IPackImmediateEntity {
+  readonly type: EEntityType.PackImmediate;
+  readonly children?: IPackImmediateEntityChildren;
+  readonly properties?: IPackImmediateEntityProperties;
+
+  constructor(packImmediate: IPackImmediateEntity) {
+    super(packImmediate);
+    this.children = packImmediate.children?.map((child: IPackImmediateEntityChild) => new PackSizeEntity(child));
+    this.properties = packImmediate.properties?.map((property: IProperty) => new Property(property));
+  }
+}
+
+export default PackImmediateEntity;
