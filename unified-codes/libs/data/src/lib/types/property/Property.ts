4bec9af6398f7c0c390a45d3741e558676b65165
Will Thomson <will.tho@outlook.com>
2020-11-05 15:14:28 +1300

    Add base Property type

---
+++ unified-codes/libs/data/src/lib/types/property/Property.ts
@@ -0,0 +1,26 @@
+export enum EPropertyField {
+  Type = 'type',
+  Value = 'value',
+};
+
+export enum EPropertyType {
+  DrugInteraction = 'drug_interaction',
+}
+
+export interface IProperty {
+  type: EPropertyType;
+  // TODO: strongly type property values.
+  value: unknown;
+}
+
+export abstract class Property implements IProperty {
+  readonly type: EPropertyType;
+  readonly value: unknown;
+
+  constructor(property: IProperty) {
+    this.type = property.type;
+    this.value = property.value;
+  }
+}
+
+export default IProperty;
