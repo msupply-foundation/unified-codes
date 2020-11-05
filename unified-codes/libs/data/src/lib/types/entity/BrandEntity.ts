c50a79b35b45e9d5380b6a7ca8c683dd0c020d8c
Will Thomson <will.tho@outlook.com>
2020-11-05 15:13:48 +1300

    Add BrandEntityType

---
+++ unified-codes/libs/data/src/lib/types/entity/BrandEntity.ts
@@ -0,0 +1,26 @@
+import { EEntityField, EEntityType, IEntity, Entity } from './Entity';
+import { IProperty, Property } from '../Property/Property';
+
+export type EBrandEntityField = EEntityField;
+
+export type IBrandEntityProperty = IProperty;
+export type IBrandEntityProperties = IBrandEntityProperty[];
+
+export interface IBrandEntity extends IEntity {
+  type: EEntityType.Brand;
+  properties?: IBrandEntityProperties;
+}
+
+export class BrandEntity extends Entity implements IBrandEntity {
+  readonly type: EEntityType.Brand;
+  readonly children: undefined;
+  readonly properties: IBrandEntityProperties;
+
+  constructor(brand: IBrandEntity) {
+    super(brand);
+    this.type = EEntityType.Brand;
+    this.properties = brand.properties?.map((property: IProperty) => new Property(property));
+  }
+}
+
+export default BrandEntity;
