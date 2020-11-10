793d480a187254f919ad55b4e02b4859c3657ff4
Mark Prins <mark@malcolmprins.net>
2020-11-10 13:26:06 +1300

    add more forms

--- unified-codes/libs/ui/src/lib/components/icons/molecules/FormIcon.tsx
+++ unified-codes/libs/ui/src/lib/components/icons/molecules/FormIcon.tsx
@@ -17,14 +17,22 @@ export const FormIcon: FormIconType = ({ form }) => {
     case 'capsule':
       return <FormCapsuleIcon height={24} width={24} />;
 
+    case 'injectable solution':
     case 'injection':
+    case 'injection, ampoule':
+    case 'injection, pre-filled syringe':
+    case 'injection, vial':
       return <FormInjectionIcon height={24} width={24} />;
 
+    case 'eye drops':
     case 'liquid':
     case 'oral liquid':
+    case 'solution':
       return <FormLiquidIcon height={24} width={24} />;
 
     case 'tablet':
+    case 'tablet, chewable':
+    case 'tablet, dispersible':
       return <FormTabletIcon height={24} width={24} />;
 
     default:
