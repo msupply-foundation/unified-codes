ed355d4c32566ad1e80356b81f7bc8876c1e3c0e
Will Thomson <will.tho@outlook.com>
2020-11-27 14:22:59 +1300

    Fix setup script typo

--- unified-codes/tools/scripts/dgraph/setup.sh
+++ unified-codes/tools/scripts/dgraph/setup.sh
@@ -7,6 +7,6 @@ echo " * Initialising demo graph schema..."
 bash ./alter.sh ./data/v2/schema.gql
 
 echo " * Populating demo graph data..."
-npx babel-node ./migration/v2/import.js ./data/v2/products_3_0.xlsm
+npx babel-node ./migration/v2/import.js ./data/v2/products.csv
 
 echo " * Done."
