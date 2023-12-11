bb3443397944c87e641cfa7bb492a81b7cc83063
Lache Melvin <lache.melvin@gmail.com>
2023-12-12 08:16:50 +1300

    add useEntity hook and query

--- frontend/system/src/api/operations.generated.ts
+++ frontend/system/src/api/operations.generated.ts
@@ -5,6 +5,8 @@ import * as Dom from 'graphql-request/dist/types.dom';
 import gql from 'graphql-tag';
 export type EntityRowFragment = { __typename: 'EntityType', type: string, description?: string | null, code: string, id: string };
 
+export type EntityDetailsFragment = { __typename: 'EntityType', code: string, description?: string | null, type: string, properties?: Array<{ __typename: 'PropertyType', type: string, value: string }> | null };
+
 export type EntitiesQueryVariables = Types.Exact<{
   filter: Types.EntitySearchInput;
   first: Types.Scalars['Int']['input'];
@@ -14,12 +16,30 @@ export type EntitiesQueryVariables = Types.Exact<{
 
 export type EntitiesQuery = { __typename: 'Query', entities: { __typename: 'EntityCollectionType', totalLength: number, data: Array<{ __typename: 'EntityType', type: string, description?: string | null, code: string, id: string }> } };
 
+export type EntityQueryVariables = Types.Exact<{
+  code: Types.Scalars['String']['input'];
+}>;
+
+
+export type EntityQuery = { __typename: 'Query', entity?: { __typename: 'EntityType', code: string, description?: string | null, type: string, children?: Array<{ __typename: 'EntityType', code: string, description?: string | null, type: string, children?: Array<{ __typename: 'EntityType', code: string, description?: string | null, type: string, children?: Array<{ __typename: 'EntityType', code: string, description?: string | null, type: string, children?: Array<{ __typename: 'EntityType', code: string, description?: string | null, type: string, children?: Array<{ __typename: 'EntityType', code: string, description?: string | null, type: string, properties?: Array<{ __typename: 'PropertyType', type: string, value: string }> | null }> | null, properties?: Array<{ __typename: 'PropertyType', type: string, value: string }> | null }> | null, properties?: Array<{ __typename: 'PropertyType', type: string, value: string }> | null }> | null, properties?: Array<{ __typename: 'PropertyType', type: string, value: string }> | null }> | null, properties?: Array<{ __typename: 'PropertyType', type: string, value: string }> | null }> | null, properties?: Array<{ __typename: 'PropertyType', type: string, value: string }> | null } | null };
+
 export const EntityRowFragmentDoc = gql`
     fragment EntityRow on EntityType {
   id: uid
   type
   description
   code
+}
+    `;
+export const EntityDetailsFragmentDoc = gql`
+    fragment EntityDetails on EntityType {
+  code
+  description
+  type
+  properties {
+    type
+    value
+  }
 }
     `;
 export const EntitiesDocument = gql`
@@ -32,6 +52,28 @@ export const EntitiesDocument = gql`
   }
 }
     ${EntityRowFragmentDoc}`;
+export const EntityDocument = gql`
+    query entity($code: String!) {
+  entity(code: $code) {
+    ...EntityDetails
+    children {
+      ...EntityDetails
+      children {
+        ...EntityDetails
+        children {
+          ...EntityDetails
+          children {
+            ...EntityDetails
+            children {
+              ...EntityDetails
+            }
+          }
+        }
+      }
+    }
+  }
+}
+    ${EntityDetailsFragmentDoc}`;
 
 export type SdkFunctionWrapper = <T>(action: (requestHeaders?:Record<string, string>) => Promise<T>, operationName: string, operationType?: string) => Promise<T>;
 
@@ -42,6 +84,9 @@ export function getSdk(client: GraphQLClient, withWrapper: SdkFunctionWrapper = 
   return {
     entities(variables: EntitiesQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<EntitiesQuery> {
       return withWrapper((wrappedRequestHeaders) => client.request<EntitiesQuery>(EntitiesDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'entities', 'query');
+    },
+    entity(variables: EntityQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<EntityQuery> {
+      return withWrapper((wrappedRequestHeaders) => client.request<EntityQuery>(EntityDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'entity', 'query');
     }
   };
 }
