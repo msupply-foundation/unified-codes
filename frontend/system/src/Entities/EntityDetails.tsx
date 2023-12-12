068250894aac1314228a88723c2f521d02780813
Lache Melvin <lache.melvin@gmail.com>
2023-12-13 10:04:03 +1300

    show/hide all codes

--- frontend/system/src/Entities/EntityDetails.tsx
+++ frontend/system/src/Entities/EntityDetails.tsx
@@ -5,10 +5,11 @@ import {
   ChevronDownIcon,
   CopyIcon,
   FlatButton,
+  Switch,
 } from '@common/ui';
 import { useBreadcrumbs, useNotification } from '@common/hooks';
 import { useEntity } from '../api';
-import { Typography } from '@mui/material';
+import { FormControlLabel, Typography } from '@mui/material';
 import { TreeItem, TreeView } from '@mui/lab';
 import { useParams } from 'react-router-dom';
 import { EntityDetailsFragment } from '../api/operations.generated';
@@ -16,9 +17,11 @@ import { EntityDetailsFragment } from '../api/operations.generated';
 type IEntity = EntityDetailsFragment & { children?: IEntity[] | null };
 
 export const EntityDetails = () => {
+  const t = useTranslation('system');
   const { code } = useParams();
   const { setSuffix } = useBreadcrumbs();
   const [expanded, setExpanded] = useState<string[]>([]);
+  const [showAllCodes, setShowAllCodes] = useState(false);
 
   const { data: entity } = useEntity(code || '');
 
@@ -27,17 +30,17 @@ export const EntityDetails = () => {
   }, [entity?.name]);
 
   useEffect(() => {
-    const codes: string[] = [];
+    const allCodes: string[] = [];
 
-    const addCodeToCodes = (ent?: IEntity | null) => {
+    const addCodeToList = (ent?: IEntity | null) => {
       if (ent) {
-        codes.push(ent.code);
-        ent.children?.forEach(addCodeToCodes);
+        allCodes.push(ent.code);
+        ent.children?.forEach(addCodeToList);
       }
     };
-    addCodeToCodes(entity);
+    addCodeToList(entity);
 
-    setExpanded(codes);
+    setExpanded(allCodes);
   }, [entity]);
 
   return (
@@ -47,9 +50,21 @@ export const EntityDetails = () => {
           paddingBottom: '16px',
           flex: 1,
           display: 'flex',
-          justifyContent: 'space-between',
+          justifyContent: 'end',
+          marginRight: 'max(0px, calc((100vw - 1200px) / 2))',
         }}
-      ></AppBarContentPortal>
+      >
+        <FormControlLabel
+          control={
+            <Switch
+              checked={showAllCodes}
+              onChange={() => setShowAllCodes(!showAllCodes)}
+            />
+          }
+          label={`${t('label.show-all-codes')}:`}
+          labelPlacement="start"
+        />
+      </AppBarContentPortal>
       <TreeView
         disableSelection
         expanded={expanded}
@@ -57,13 +72,19 @@ export const EntityDetails = () => {
         defaultCollapseIcon={<ChevronDownIcon />}
         onNodeToggle={(_, codes: string[]) => setExpanded(codes)}
       >
-        <EntityTreeItem entity={entity} />
+        <EntityTreeItem showAllCodes={showAllCodes} entity={entity} />
       </TreeView>
     </>
   );
 };
 
-const EntityTreeItem = ({ entity }: { entity?: IEntity | null }) => {
+const EntityTreeItem = ({
+  entity,
+  showAllCodes,
+}: {
+  showAllCodes: boolean;
+  entity?: IEntity | null;
+}) => {
   const t = useTranslation('system');
   const { success } = useNotification();
 
@@ -80,24 +101,32 @@ const EntityTreeItem = ({ entity }: { entity?: IEntity | null }) => {
     success(t('message.code-copied-to-clipboard', { code: entity.code }))();
   };
 
+  const isLeaf = !entity.children?.length;
+  const showCode = showAllCodes || isLeaf;
+
   return (
     <TreeItem
       sx={{ paddingY: '5px' }}
       nodeId={entity.code}
       label={
-        <Typography>
-          {entity.name} -
-          <FlatButton
-            endIcon={<CopyIcon />}
-            sx={{ padding: 0 }}
-            label={entity.code}
-            onClick={handleCopyToClipboard}
-          />
+        <Typography sx={{ height: '26px' }}>
+          {entity.name}{' '}
+          {showCode && (
+            <>
+              -
+              <FlatButton
+                endIcon={<CopyIcon />}
+                sx={{ padding: 0 }}
+                label={entity.code}
+                onClick={handleCopyToClipboard}
+              />
+            </>
+          )}
         </Typography>
       }
     >
       {entity.children?.map(c => (
-        <EntityTreeItem entity={c} key={c.code} />
+        <EntityTreeItem entity={c} key={c.code} showAllCodes={showAllCodes} />
       ))}
       {entity.properties && (
         <TreeItem
