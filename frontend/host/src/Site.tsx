8e8ff927d5d8202e9688744981a32a70462f6a6f
Lache Melvin <lache.melvin@gmail.com>
2023-12-20 08:40:49 +1300

    move paper wrapper to site component

--- frontend/host/src/Site.tsx
+++ frontend/host/src/Site.tsx
@@ -13,6 +13,7 @@ import {
   useGetPageTitle,
   DetailPanel,
   Navigate,
+  Paper,
   useAuthContext,
 } from '@uc-frontend/common';
 import { AppBar, AppDrawer, Footer, NotFound } from './components';
@@ -41,38 +42,62 @@ export const Site: FC = () => {
         <Box flex={1} display="flex" flexDirection="column" overflow="hidden">
           <AppBar />
           <Box display="flex" flex={1} overflow="auto" paddingX={'24px'}>
-            <Routes>
-              <Route
-                path={RouteBuilder.create(AppRoute.Browse)
-                  .addWildCard()
-                  .build()}
-                element={<EntitiesRouter />}
-              />
-              <Route
-                path={RouteBuilder.create(AppRoute.Admin).addWildCard().build()}
-                element={
-                  <RequireAuthentication>
-                    <AdminRouter />
-                  </RequireAuthentication>
-                }
-              />
-              <Route
-                path={RouteBuilder.create(AppRoute.Settings)
-                  .addWildCard()
-                  .build()}
-                element={<Settings />}
-              />
-              <Route
-                path={RouteBuilder.create(AppRoute.Home).build()}
-                element={
-                  <Navigate
-                    to={RouteBuilder.create(AppRoute.Browse).build()}
-                    replace={true}
+            <Paper
+              sx={{
+                backgroundColor: 'background.menu',
+                borderRadius: '16px',
+                flex: 1,
+                margin: '10px auto',
+                maxWidth: '1200px',
+                padding: '16px',
+                width: '100%',
+              }}
+            >
+              <Box
+                sx={{
+                  backgroundColor: 'white',
+                  display: 'flex',
+                  borderRadius: '16px',
+                  paddingX: '16px',
+                  maxHeight: '100%',
+                }}
+              >
+                <Routes>
+                  <Route
+                    path={RouteBuilder.create(AppRoute.Browse)
+                      .addWildCard()
+                      .build()}
+                    element={<EntitiesRouter />}
                   />
-                }
-              />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
+                  <Route
+                    path={RouteBuilder.create(AppRoute.Admin)
+                      .addWildCard()
+                      .build()}
+                    element={
+                      <RequireAuthentication>
+                        <AdminRouter />
+                      </RequireAuthentication>
+                    }
+                  />
+                  <Route
+                    path={RouteBuilder.create(AppRoute.Settings)
+                      .addWildCard()
+                      .build()}
+                    element={<Settings />}
+                  />
+                  <Route
+                    path={RouteBuilder.create(AppRoute.Home).build()}
+                    element={
+                      <Navigate
+                        to={RouteBuilder.create(AppRoute.Browse).build()}
+                        replace={true}
+                      />
+                    }
+                  />
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </Box>
+            </Paper>
           </Box>
           <AppFooter />
           <AppFooterPortal SessionDetails={<Footer />} />
