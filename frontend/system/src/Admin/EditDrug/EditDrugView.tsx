bd6e43db72fe29d26114a28f64acf852f15883d0
Lache Melvin <lache.melvin@gmail.com>
2023-12-20 15:46:56 +1300

    query for entity data, set breadcrumb suffix

--- frontend/system/src/Admin/EditDrug/EditDrugView.tsx
+++ frontend/system/src/Admin/EditDrug/EditDrugView.tsx
@@ -1,5 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'frontend/common/src';
+import { useBreadcrumbs } from '@common/hooks';
+import { useEntity } from '../../Entities/api';
 
 export const EditDrugView = () => {
-  return <>Hello</>;
+  const { code } = useParams();
+  const { setSuffix } = useBreadcrumbs();
+
+  const { data: entity } = useEntity(code || '');
+
+  useEffect(() => {
+    if (entity?.name) setSuffix(entity.name);
+  }, [entity?.name]);
+  return <>{entity?.name} edit form</>;
 };
