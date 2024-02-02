cd816611590f8e2553a7be79aeabae6c66caede7
James Brunskill <james@sussol.net>
2024-02-02 17:38:43 +1300

    Upsert Error test cases

--- backend/service/src/drug_interactions/tests/update_interaction.rs
+++ backend/service/src/drug_interactions/tests/update_interaction.rs
@@ -254,4 +254,139 @@ mod test {
             .await
             .unwrap();
     }
+
+    #[actix_rt::test]
+    async fn upsert_drug_interaction_errors() {
+        let (_, _, connection_manager, _) =
+            setup_all("upsert_drug_interaction_errors", MockDataInserts::none()).await;
+
+        let service_provider = Arc::new(ServiceProvider::new(
+            connection_manager,
+            get_test_settings(""),
+        ));
+        let context = ServiceContext::as_server_admin(service_provider.clone()).unwrap();
+        let service = &context.service_provider.drug_interaction_service;
+
+        let new_interaction_group_id_1 = uuid();
+        let group_name = uuid();
+        let input = UpsertDrugInteractionGroup {
+            interaction_group_id: new_interaction_group_id_1.clone(),
+            name: group_name.clone(),
+            description: Some("Description".to_string()),
+            drugs: vec!["c9896104".to_string()],
+        };
+
+        let result = service
+            .upsert_drug_interaction_group(service_provider.clone(), context.user_id.clone(), input)
+            .await
+            .unwrap();
+        assert_eq!(result, 1);
+
+        let new_interaction_group_id_2 = uuid();
+        let group_name = uuid();
+        let input = UpsertDrugInteractionGroup {
+            interaction_group_id: new_interaction_group_id_2.clone(),
+            name: group_name.clone(),
+            description: Some("Description".to_string()),
+            drugs: vec!["06111454".to_string()],
+        };
+
+        let result = service
+            .upsert_drug_interaction_group(service_provider.clone(), context.user_id.clone(), input)
+            .await
+            .unwrap();
+        assert_eq!(result, 1);
+
+        let new_interaction_id = uuid();
+
+        let input = UpsertDrugInteraction {
+            interaction_id: new_interaction_id.clone(),
+            name: new_interaction_id.clone(),
+            description: "upsert_drug_interaction_errors".to_string(),
+            severity: dgraph::DrugInteractionSeverity::NothingExpected,
+            action: String::new(),
+            reference: String::new(),
+            drug_code_1: None,
+            drug_code_2: None,
+            interaction_group_id_1: Some(new_interaction_group_id_1.clone()),
+            interaction_group_id_2: Some(new_interaction_group_id_2.clone()),
+        };
+
+        let result = service
+            .upsert_drug_interaction(service_provider.clone(), context.user_id.clone(), input)
+            .await
+            .unwrap();
+        assert_eq!(result, 1);
+
+        // 1. 2 drugs 1 group (should be an error)
+
+        let input = UpsertDrugInteraction {
+            interaction_id: new_interaction_id.clone(),
+            name: new_interaction_id.clone(),
+            description: "upsert_drug_interaction_errors".to_string(),
+            severity: dgraph::DrugInteractionSeverity::NothingExpected,
+            action: String::new(),
+            reference: String::new(),
+            drug_code_1: Some("5b66ffca".to_string()),
+            drug_code_2: Some("c24cdb96".to_string()),
+            interaction_group_id_1: Some(new_interaction_group_id_1.clone()),
+            interaction_group_id_2: None,
+        };
+
+        let result = service
+            .upsert_drug_interaction(service_provider.clone(), context.user_id.clone(), input)
+            .await;
+        assert!(result.is_err());
+
+        // 2. 1 drugs no groups (should be an error)
+
+        let input = UpsertDrugInteraction {
+            interaction_id: new_interaction_id.clone(),
+            name: new_interaction_id.clone(),
+            description: "upsert_drug_interaction_errors".to_string(),
+            severity: dgraph::DrugInteractionSeverity::NothingExpected,
+            action: String::new(),
+            reference: String::new(),
+            drug_code_1: Some("5b66ffca".to_string()),
+            drug_code_2: None,
+            interaction_group_id_1: None,
+            interaction_group_id_2: None,
+        };
+
+        let result = service
+            .upsert_drug_interaction(service_provider.clone(), context.user_id.clone(), input)
+            .await;
+        assert!(result.is_err());
+
+        // Clean Up time!
+
+        // Delete the  interaction
+        let _result = service
+            .delete_drug_interaction(
+                service_provider.clone(),
+                context.user_id.clone(),
+                new_interaction_id.clone(),
+            )
+            .await
+            .unwrap();
+
+        // Delete the groups
+        let _result = service
+            .delete_drug_interaction_group(
+                service_provider.clone(),
+                context.user_id.clone(),
+                new_interaction_group_id_1.clone(),
+            )
+            .await
+            .unwrap();
+
+        let _result = service
+            .delete_drug_interaction_group(
+                service_provider.clone(),
+                context.user_id.clone(),
+                new_interaction_group_id_2.clone(),
+            )
+            .await
+            .unwrap();
+    }
 }
